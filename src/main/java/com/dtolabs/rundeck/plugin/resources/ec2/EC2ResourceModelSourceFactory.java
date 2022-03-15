9b74e4a86c0a4e6d190dcc544c0e2da851bb9631
Jake Cohen <jnohec@gmail.com>
2022-03-14 19:42:49 -0700

    modify field description for clarity

--- src/main/java/com/dtolabs/rundeck/plugin/resources/ec2/EC2ResourceModelSourceFactory.java
+++ src/main/java/com/dtolabs/rundeck/plugin/resources/ec2/EC2ResourceModelSourceFactory.java
@@ -134,7 +134,7 @@ public class EC2ResourceModelSourceFactory implements ResourceModelSourceFactory
                                                 ))
             .property(PropertyUtil.string(ENDPOINT, "Endpoint", "AWS EC2 Endpoint to specify region, or blank for default. Include comma-separated list of endpoints to integrate with multiple regions.\n\n" +
                     "Example: `https://ec2.us-west-1.amazonaws.com, https://ec2.us-east-1.amazonaws.com` This would retrieve instances from the `US-WEST-1` and `US-EAST-1` regions.\n" +
-                            "Optionally use `ALL_REGIONS` to automatically pull in instances from all regions (except GovCloud).",
+                            "Optionally use `ALL_REGIONS` to automatically pull in instances from all regions that the AWS credentials (or IAM Role) have access to.",
                     false,
                     null))
             .property(PropertyUtil.string(HTTP_PROXY_HOST, "HTTP Proxy Host", "HTTP Proxy Host Name, or blank for default", false, null))
